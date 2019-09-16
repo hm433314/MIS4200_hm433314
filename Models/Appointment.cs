8e1977bbbc2c89e0cf7fa8cae0dcf12bc3d22049
hm433314 <hm433314@ohio.edu>
2019-09-16 13:45:50 -0400

    Appointment and Doctor

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MIS4200_hm433314.Models
 {
     public class Appointment
-    {public int PatientID { get; set; }
-     public int DoctorID{ get; set; }
+    {[Key] public int AppointmentID { get; set; }
      public DateTime AppointmentDate{ get; set; }
      public string AppointmentReason{ get; set; }
+     
+     public int DoctorID { get; set; }   
+     public virtual Doctor Doctor { get; set; }
+
+     public int PatientID { get; set; }
+     public virtual Patient Patient { get; set; }
+
     }
 }
---
+++ Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_hm433314.Models
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string DFName { get; set; }
+        public string DLName { get; set; }
+        public string DEmail { get; set; }
+        public string DPhone { get; set; }
+
+        public ICollection<Appointment> Appointment { get; set; }
+    }
+}
