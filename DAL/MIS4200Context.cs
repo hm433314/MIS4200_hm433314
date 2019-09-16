68087a1635f6e71b0117cff33a7f84fc93190ff7
hm433314 <hm433314@ohio.edu>
2019-09-16 14:32:43 -0400

    Building Controllers

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MIS4200_hm433314.Models;
+using System.Data.Entity;
+
+namespace MIS4200_hm433314.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public  MIS4200Context(): base ("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
