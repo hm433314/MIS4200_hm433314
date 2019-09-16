947588bdac54e6ab0811ec33023d722730fc011a
hm433314 <hm433314@ohio.edu>
2019-09-16 14:52:21 -0400

    Refresh Changes

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -8,6 +8,7 @@ namespace MIS4200_hm433314.Models
 {
     public class Appointment
     {[Key] public int AppointmentID { get; set; }
+     
      public DateTime AppointmentDate{ get; set; }
      public string AppointmentReason{ get; set; }
      
