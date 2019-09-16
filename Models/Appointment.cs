fec6033a7c81db1d0fc26ef20c08a7e52dc3e28d
hm433314 <hm433314@ohio.edu>
2019-09-16 10:33:19 -0400

    appointment changes

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -6,6 +6,9 @@ using System.Web;
 namespace MIS4200_hm433314.Models
 {
     public class Appointment
-    {
+    {public int PatientID { get; set; }
+     public int DoctorID{ get; set; }
+     public DateTime AppointmentDate{ get; set; }
+     public string AppointmentReason{ get; set; }
     }
 }
